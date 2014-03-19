3404f06cd5c13b879830fe884425475dc5439224
robinto <al4crity@gmail.com>
2014-03-20 00:18:53 +0100

    Faulty labels for model fields.

--- chess/simpleChess/models.py
+++ chess/simpleChess/models.py
@@ -4,8 +4,10 @@ from django.forms import ModelForm
 # Create your models here.
 class ChessGame(models.Model):
 	fen = models.CharField(max_length=100)
+	pgn = models.CharField(max_length=600)
 
 class ChessForm(ModelForm):
 	class Meta:
 		model = ChessGame
-		field = ['fen']
+		fenField = ['fen']
+		pgnField = ['pgn']
